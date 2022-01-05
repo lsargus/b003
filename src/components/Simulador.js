d41a1773929cc9c1cbe4b624847413f058009718
Lucas Argus <46332051+lsargus@users.noreply.github.com>
2022-01-05 16:51:45 -0300

    tratamento request

--- src/components/Simulador.js
+++ src/components/Simulador.js
@@ -1,20 +1,22 @@
 import { Component } from "react/cjs/react.production.min";
 import {estadoValvula} from "./SimuladorRequest"
 
+
+ 
 class Simulador extends Component{
 
     constructor() {
         super();
 
         document.addEventListener('DOMContentLoaded',()=>{
-
-
-            var tela = document.querySelector("canvas");
-            const context = tela.getContext('2d');
         
+            var tela = this.getTela()
+
             tela.width = 700;
             tela.height = 500;
         
+            var context = this.getContext()
+
             //desenha o quadrado azul 
             context.beginPath();
             context.fillStyle= "blue";
@@ -74,98 +76,107 @@ class Simulador extends Component{
             context.fill();
             context.closePath();
         
-            // desenha um quadradado branco conforme vá decrementando o nivel
-            const decrementaNivel = (decremento) =>{
-        
-                if(decremento <= 200){
-                    context.clearRect(300, 100, 150, decremento);   
-                    context.closePath(); 
-        
-                }
-        
-            }
-        
-            //Aumenta o quadrado branco conforme vá aumentando
-            const aumentaNivel = (aumenta) =>{
-        
-                if(aumenta >= 200){
-                    context.clearRect(300, 100, 150, aumenta);   
-                    context.closePath(); 
-        
-                }
-        
-            }
-        
-           // OnV1();
-        
-        // se a valvula estiver no estado aberto (true), sua representação muda para vermelho
-            const valvula1 = (estado) => {
-        
-                if(estado === true){
-        
-                    context.fillStyle= "red";
-                    context.beginPath();
-                    context.moveTo(240,150);
-                    context.lineTo(199,125);
-                    context.lineTo(240  ,100);
-                    context.stroke();
-                    context.fill();
-                    context.closePath();
-                    
-                    context.fillStyle= "red";
-                    context.beginPath();
-                    context.moveTo(160,150);
-                    context.lineTo(202,125);
-                    context.lineTo(160,100);
-                    context.stroke();
-                    context.fill();
-                    context.closePath();
-        
-                    aumentaNivel(150);
-        
-                } 
-            }
-        
-           //var v1 = verficaEStadoValvula();
-            valvula1(true);
-        
-            // se a valvula estiver no estado aberto (true), sua representação muda para vermelho
-            const valvula2 = (estado) => {
-        
-                if(estado === true){
-        
-                    context.fillStyle= "red";
-                    context.beginPath();
-                    context.moveTo(600,305);
-                    context.lineTo(559  ,280);
-                    context.lineTo(600  ,255    );
-                    context.stroke();
-                    context.fill();
-                    context.closePath();
-                    
-                    context.fillStyle= "red";
-                    context.beginPath();
-                    context.moveTo(520,305);
-                    context.lineTo(562  ,280);
-                    context.lineTo(520  ,255);
-                    context.stroke();
-                    context.fill();
-                    context.closePath();
-        
-                    decrementaNivel(150);
-        
-                } 
-            }
-            valvula2(false);
-        
-        
-        
         });
+        this.atualizaValvulas()
     }
+
+    // desenha um quadradado branco conforme vá decrementando o nivel
+    decrementaNivel(decremento) {
+        var context = this.getContext()
+        if(decremento <= 200){
+            context.clearRect(300, 100, 150, decremento);   
+            context.closePath(); 
+
+        }
+
+    }
+
+    //Aumenta o quadrado branco conforme vá aumentando
+    aumentaNivel(aumenta) {
+        var context = this.getContext()
+        if(aumenta >= 200){
+            context.clearRect(300, 100, 150, aumenta);   
+            context.closePath(); 
+
+        }
+
+    }
+    // se a valvula estiver no estado aberto (true), sua representação muda para vermelho
+ valvula1(estado) {
+        
+    if(estado === true){
+        var context = this.getContext()
+        context.fillStyle= "red";
+        context.beginPath();
+        context.moveTo(240,150);
+        context.lineTo(199,125);
+        context.lineTo(240  ,100);
+        context.stroke();
+        context.fill();
+        context.closePath();
+        
+        context.fillStyle= "red";
+        context.beginPath();
+        context.moveTo(160,150);
+        context.lineTo(202,125);
+        context.lineTo(160,100);
+        context.stroke();
+        context.fill();
+        context.closePath();
+
+        this.aumentaNivel(150);
+
+    } 
+}
+
+// se a valvula estiver no estado aberto (true), sua representação muda para vermelho
+valvula2 (estado) {
+        
+    if(estado === true){
+        var context = this.getContext()
+        context.fillStyle= "red";
+        context.beginPath();
+        context.moveTo(600,305);
+        context.lineTo(559  ,280);
+        context.lineTo(600  ,255    );
+        context.stroke();
+        context.fill();
+        context.closePath();
+        
+        context.fillStyle= "red";
+        context.beginPath();
+        context.moveTo(520,305);
+        context.lineTo(562  ,280);
+        context.lineTo(520  ,255);
+        context.stroke();
+        context.fill();
+        context.closePath();
+
+        this.decrementaNivel(150);
+
+    } 
+}
     getCanvas = elem => {
         this.canvas = elem
-      }
+    }
 
+    atualizaValvulas() {
+        setInterval(()=> {
+            const resp = estadoValvula()
+            resp.then(v => {
+                console.log(v.first_name)
+                this.valvula1(v.valvula_1)
+                this.valvula2(v.valvula_2) })
+        }
+        , 10000);
+    }
+
+    getTela() {
+        return document.querySelector("canvas");
+    }
+    getContext() {return document.querySelector("canvas").getContext('2d'); }
+
+    
      
     render(
         ){
--- src/components/SimuladorRequest.js
+++ src/components/SimuladorRequest.js
@@ -1,9 +1,11 @@
 
 
-export function estadoValvula() {
+export async function estadoValvula() {
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Access-Control-Allow-Origin", "*");
+    myHeaders.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     
 
     var requestOptions = {
@@ -12,9 +14,8 @@ export function estadoValvula() {
       redirect: 'follow'
     };
     
-    fetch("http://127.0.0.1:5000/estadoVavula", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+    return fetch("http://127.0.0.1:5000/estadoVavula", requestOptions).then(response => {
+      return response.json()
+    });
   
   }
